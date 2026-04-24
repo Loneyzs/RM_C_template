dd5fcbc9bb39186f116ff3b21a8c1bdcb8772c8a
Loneyzs <a77431005@163.com>
2026-04-24 18:06:38 +0800

    初步完成模板开发

--- src/main.c
+++ src/main.c
@@ -7,25 +7,8 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#include "unit_tests.h"
-
-LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
-
 int main(void)
 {
-	int rc;
-
-	LOG_INF("=== IMU JustFloat minimal test ===");
-	k_msleep(200);
-
-	rc = test_imu_justfloat_start();
-	if (rc < 0) {
-		LOG_ERR("IMU JustFloat test start failed: %d", rc);
-	}
-
-	while (1) {
-		k_msleep(1000);
-	}
-
+    
 	return 0;
 }
